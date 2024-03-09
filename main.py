287a1e3f316034f6c5850da64afbeb88fad1925f
garnetraven <corbinchristopher1@gmail.com>
2024-03-09 11:16:43 -0700

    add pdf conversion

--- main.py
+++ main.py
@@ -5,30 +5,15 @@ import os
 import discord
 from discord.ext import commands
 
-client = commands.Bot(
-    intents=discord.Intents.all(),
-    command_prefix='!'
-)
+intents = discord.Intents.all()
+client = commands.Bot(intents=intents, command_prefix='!')
 
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
+    await client.tree.sync(guild=discord.Object(id=int(os.getenv('GUILD_ID'))))
 
-    # register application command tree with Discord 
-    await client.tree.sync(guild=discord.Object(id=int(os.getenv('GUILD_ID')))) # register for only specific guild to decrease delay
-
-# EXAMPLE: OLD PREFIX COMMAND
-@client.command()
-async def prefixtest(ctx: commands.Context):
-    await ctx.channel.send(f'Hi {ctx.author.name}! Welcome to {ctx.guild.name}.')
-
-# EXAMPLE: SLASH COMMAND
-@client.tree.command(
-    name='slashtest',
-    description='A test slash command',
-    guild=discord.Object(id=int(os.getenv('GUILD_ID'))) # register for only specific guild to decrease delay
-)
-async def slashtest(interaction: discord.interactions.Interaction):
-    await interaction.response.send_message(f'Hi {interaction.user.name}! Welcome to {interaction.guild.name}.')
+# Load the PDFConverter cog
+client.load_extension('pdf_converter')
 
 client.run(os.getenv('DISCORD_BOT_TOKEN'))
