c1fc0e06d14d777ca37f95cba69a0b89ce4ebf02
garnetraven <corbinchristopher1@gmail.com>
2024-03-09 15:08:21 -0700

    change to listen in channel instead of calling function

--- cogs/pdf_converter.py
+++ cogs/pdf_converter.py
@@ -7,38 +7,41 @@ class PDFConverter(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def convert(self, ctx, message_id: int):
-        print("Command received.")
-        message = await ctx.channel.fetch_message(message_id)
-        print("Message fetched with ID: {messagee_id}")
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author == self.bot.user:
+            return
 
         for attachment in message.attachments:
             if attachment.filename.endswith('.pdf'):
-                print("PDF attachment found.")
-                if not os.path.exists('/tmp'):
-                    os.makedirs('/tmp')
+                await self.convert(message, attachment)
 
-                pdf_path = f'/tmp/{attachment.filename}'
-                await attachment.save(pdf_path)
-                print(f"PDF saved to {pdf_path}")
+    async def convert(self, message, attachment):
+        print("PDF attachment found.")
+        if not os.path.exists('/tmp'):
+            os.makedirs('/tmp')
 
-                print("Converting PDF to images...")
-                try:
-                    images = convert_from_path(pdf_path)
-                    print(f"PDF converted to {len(images)} images.")
-                except Exception as e:
-                    print(f"An error occurred while converting the PDF to images: {e}")
-                    return
+        pdf_path = f'/tmp/{attachment.filename}'
+        await attachment.save(pdf_path)
+        print(f"PDF saved to {pdf_path}")
 
-                for i, image in enumerate(images):
-                    image_path = f'/tmp/image{i}.jpeg'
-                    image.save(image_path, 'JPEG')
-                    print(f"Image saved to {image_path}")
+        print("Converting PDF to images...")
+        try:
+            images = convert_from_path(pdf_path)
+            print(f"PDF converted to {len(images)} images.")
+        except Exception as e:
+            print(f"An error occurred while converting the PDF to images: {e}")
+            return
 
-                    with open(image_path, 'rb') as img:
-                        await ctx.send(file=discord.File(img, 'image.jpeg'))
-                        print(f"Image sent to Discord.")
+        for i, image in enumerate(images):
+            image_path = f'/tmp/image{i}.jpeg'
+            image.save(image_path, 'JPEG')
+            print(f"Image saved to {image_path}")
+
+            with open(image_path, 'rb') as img:
+                await message.channel.send(file=discord.File(img, 'image.jpeg'))
+                print(f"Image sent to Discord.")
 
 async def setup(bot):
     await bot.add_cog(PDFConverter(bot))
+    
